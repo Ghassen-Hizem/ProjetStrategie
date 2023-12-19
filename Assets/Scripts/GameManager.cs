ed94fbe93d947a3c9e6111da1861e07afbd506f2
sarra ksontini <sarraksontini@gmail.com>
2023-12-19 14:45:06 +0100

    init project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    Button button;
+    private int idArena;
+    [SerializeField] private GameObject canvaArenaChoice;
+    [SerializeField] private GameObject canvaMainMenu;
+    public void ArenaChoice(Button button)
+    {
+        if (button.name == "Arena1")
+        {
+            idArena = 1;
+        }
+        if (button.name == "Arena2")
+        {
+            idArena = 2;
+        }
+        if (button.name == "Arena3")
+        {
+            idArena = 3;
+        }
+        if (button.name == "Arena4")
+        {
+            idArena = 4;
+        }
+        if (button.name == "Arena5")
+        {
+            idArena = 5;
+        }
+    }
+    
+    public void playButton()
+    {
+        canvaArenaChoice.SetActive(true);
+        canvaMainMenu.SetActive(false);
+    }
+    public void startGame()
+    {
+        SceneManager.LoadSceneAsync(idArena);
+    }
+}
