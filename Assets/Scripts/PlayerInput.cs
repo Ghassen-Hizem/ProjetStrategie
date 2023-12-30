4bd8a219f019a98674839e8ea2853a00393b825d
sarra ksontini <sarraksontini@gmail.com>
2023-12-30 16:15:40 +0100

    player selection done

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    [SerializeField] private Camera camera;
+    [SerializeField] private Camera Camera;
     [SerializeField] private RectTransform selectionBox;
     [SerializeField] private LayerMask unitLayers;
     [SerializeField] private LayerMask floorLayers;
 
+    private float DragDelay = 0.1f;
+
+    private float MouseDownTime;
     private Vector2 startMousePosition;
 
     private void Update()
     {
         HandleSelectionInputs();
+        HandleMovementInputs();
     }
 
+    private void HandleMovementInputs()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse1) && SelectionManager.Instance.SelectedUnits.Count > 0)
+        {
+            if (Physics.Raycast(Camera.ScreenPointToRay(Input.mousePosition), out RaycastHit Hit, floorLayers))
+            {
+                foreach(SelectableUnit unit in SelectionManager.Instance.SelectedUnits)
+                {
+                    unit.MoveTo(Hit.point);
+                }
+            }
+        }
+    }
     private void HandleSelectionInputs()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
@@ -23,8 +40,9 @@ public class PlayerInput : MonoBehaviour
             selectionBox.sizeDelta = Vector2.zero;
             selectionBox.gameObject.SetActive(true);
             startMousePosition = Input.mousePosition;
+            MouseDownTime = Time.time;
         }
-        else if(Input.GetKey(KeyCode.Mouse0))
+        else if(Input.GetKey(KeyCode.Mouse0) && MouseDownTime + DragDelay < Time.time)
         {
             ResizeSelectionBox();
         }
@@ -32,6 +50,33 @@ public class PlayerInput : MonoBehaviour
         {
             selectionBox.sizeDelta = Vector2.zero;
             selectionBox.gameObject.SetActive(false);
+
+            if (Physics.Raycast(Camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, unitLayers) && hit.collider.TryGetComponent<SelectableUnit>(out SelectableUnit unit))
+            {
+                if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                {
+                    if(SelectionManager.Instance.isSelected(unit))
+                    {
+                        SelectionManager.Instance.Deselect(unit);
+                    }
+                    else
+                    {
+                        SelectionManager.Instance.Select(unit);
+                    }
+                }
+                else
+                {
+                    SelectionManager.Instance.DeselectAll();
+                    SelectionManager.Instance.Select(unit);
+                }
+            }
+            else if (MouseDownTime + DragDelay > Time.time)
+            {
+                SelectionManager.Instance.DeselectAll();
+            }
+
+
+            MouseDownTime = 0;
         }
 
        
@@ -44,5 +89,24 @@ public class PlayerInput : MonoBehaviour
 
         selectionBox.anchoredPosition = startMousePosition + new Vector2(width / 2, height / 2);
         selectionBox.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
+
+        Bounds bounds = new Bounds(selectionBox.anchoredPosition, selectionBox.sizeDelta);
+
+        for (int i = 0; i < SelectionManager.Instance.AvailableUnits.Count; i++)
+        {
+            if (UnitIsInSelectionBox(Camera.WorldToScreenPoint(SelectionManager.Instance.AvailableUnits[i].transform.position), bounds)) 
+            {
+                SelectionManager.Instance.Select(SelectionManager.Instance.AvailableUnits[i]);
+            }
+            else
+            {
+                SelectionManager.Instance.Deselect(SelectionManager.Instance.AvailableUnits[i]);
+            }
+        }
+    }
+
+    private bool UnitIsInSelectionBox(Vector2 Position, Bounds Bounds)
+    {
+        return Position.x > Bounds.min.x && Position.x < Bounds.max.x && Position.y > Bounds.min.y && Position.y < Bounds.max.y;
     }
 }
