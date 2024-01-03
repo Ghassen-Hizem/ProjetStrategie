fa82d473cbd2ba0dc71ff2a6e7b87a412c80fda3
hizem <ghassen.hizem@supcom.tn>
2024-01-03 20:04:02 +0100

    création du sable

--- Assets/Scripts/SceneA3/SableScript.cs
+++ Assets/Scripts/SceneA3/SableScript.cs
@@ -7,12 +7,30 @@ public class SableScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+         
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // SOLVED , REMOVED KINEMATIC BODY
+    private void OnTriggerEnter(Collider other) {
+       
+        if(other.CompareTag("Player")) {
+            
+             other.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().speed = 2f;
+            
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+       
+        if(other.CompareTag("Player")) {
+            
+             other.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().speed = 10f;
+            
+        }
     }
 }
